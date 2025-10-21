020f49e89fdeefdc15d3693ea2982ce2ac1e0a31
rovenso29 <roberta.venso@escola.pr.gov.br>
2025-10-21 09:23:20 -0300

    21-10

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave;")
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave;");
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+
+function mostraPalavrasChave(){
+    alert("Fui clicado")
+}
