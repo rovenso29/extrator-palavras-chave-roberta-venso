6d09afd7d1b379437cd5ba43c6f3c08ee6f034af
rovenso29 <roberta.venso@escola.pr.gov.br>
2025-10-28 09:36:41 -0300

    28-10

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave(){
-    const text = document.querySelector("#entrada-de-texto").value;
+    const texto = document.querySelector("#entrada-de-texto").value;
     const campoResultado = document.querySelector("#resultado-palavrachave");
     const palavrasChave = processaTexto(texto);
 
